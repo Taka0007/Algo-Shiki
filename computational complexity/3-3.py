fff37b73fbab3450eebfb7404cf533ee44db5df0
Taka0007 <7takacode@gmail.com>
2023-10-12 15:34:31 +0900

    delete

--- computational complexity/3-3.py
+++ computational complexity/3-3.py
@@ -1,5 +1,8 @@
-N = int(input())
-A = list(map(int,input().split()))
+N,M = map(int,input().split())
+ans = 0
 
-ans = sum(A) - min(A)
+for x in range(1,N+1):
+    for y in range(1,N+1):
+        if M - (x+y) >= 1:
+            ans += (M - (x+y))
 print(ans)
